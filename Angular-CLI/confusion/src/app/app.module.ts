f4a944ce4f2a59f1af2bd0ed0c5c569eff6b3cfe
vis-8757 <runhopperrun@gmail.com>
2019-06-25 03:40:23 +0530

    Reactive forms with validation

--- Angular-CLI/confusion/src/app/app.module.ts
+++ Angular-CLI/confusion/src/app/app.module.ts
@@ -29,6 +29,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -46,7 +49,10 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
-    MatFormFieldModule, 
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule, 
     MatInputModule,
     MatCheckboxModule,
     BrowserAnimationsModule,
--- Angular-CLI/confusion/src/app/contact/contact.component.ts
+++ Angular-CLI/confusion/src/app/contact/contact.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+//ViewChild to get access to all child elements in the DOM
+import { FormBuilder, FormGroup, Validators, FormControl, FormGroupDirective } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
+
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(FormGroupDirective) ffy:FormGroupDirective ;
+  //refering to feedback form by a var fform to get access to it to be able to completely reset it.
 
-  ngOnInit() {
-  }
+  feedbackForm: FormGroup;
+  feedback: Feedback;
+  contactType = ContactType;
+  constructor(private fb:FormBuilder) {
+    this.createForm();
+   }
+//createForm can be defined in the constructior itself but we might need the 
+//property elsewhere too so its better to define it separately.
+
+ngOnInit() {}
+
+  createForm() {
+    this.feedbackForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      telnum: ['', Validators.required],
+      email: ['', Validators.required],   
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+//this creates a form that then needs to be mapped into the template code as well
+
+}
+
+onSubmit() {
+  this.feedback = this.feedbackForm.value;
+  //form model (feedback.ts) is exactly the same as data model (conact.comp.ts file) so 
+  //the value of form model is difrectly taken.
+  // The feedbackForm gives a property called value, which allows  to retrieve the current value
+  // of all these from my feedback form. So this will form a JavaScript object, which  can 
+  //then be mapped into the feedback JavaScript object.
+
+  console.log(this.feedback);
+  this.feedbackForm.reset({
+    firstname: '',
+    lastname: '',
+    telnum: '',
+    email: '',
+    agree: false,
+    contacttype: 'None',
+    message: ''
+  });
+// we put it like the same object as in create form so that it returns to exactly same stata upon submit
+
+this.ffy.resetForm();
+}
 
 }
